ab7da69cfefba39467a3ee85bb4e752ffed14c45
Mohamed Osama <mohamed.osama200538@gmail.com>
2026-07-26 08:57:44 +0300

    refactor main icons in app config screen & onBoarding

--- lib/features/app_config/widgets/app_config_hero_widget.dart
+++ lib/features/app_config/widgets/app_config_hero_widget.dart
@@ -17,31 +17,51 @@ class AppConfigHeroWidget extends StatelessWidget {
           height: 180,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
-            gradient: LinearGradient(
-              colors: [
-                colorScheme.primaryContainer,
-                colorScheme.secondaryContainer,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
+            color: colorScheme.primary.withValues(alpha: 0.1),
             border: Border.all(
-              color: colorScheme.primary.withValues(alpha: 0.2),
+              color: colorScheme.primary.withValues(alpha: 0.25),
               width: 2,
             ),
             boxShadow: [
               BoxShadow(
-                color: colorScheme.primary.withValues(alpha: 0.18),
-                blurRadius: 36,
+                color: colorScheme.primary.withValues(alpha: 0.12),
+                blurRadius: 30,
                 spreadRadius: 4,
-                offset: const Offset(0, 12),
+                offset: const Offset(0, 10),
               ),
             ],
           ),
-          child: Icon(
-            IconsaxPlusBold.send_2,
-            size: 96,
-            color: colorScheme.onPrimary,
+          child: Center(
+            child: Container(
+              width: 128,
+              height: 128,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  colors: [
+                    colorScheme.primary,
+                    colorScheme.primary.withValues(alpha: 0.88),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: colorScheme.primary.withValues(alpha: 0.35),
+                    blurRadius: 20,
+                    spreadRadius: 2,
+                    offset: const Offset(0, 6),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Icon(
+                  IconsaxPlusBold.send_2,
+                  size: 96,
+                  color: colorScheme.onPrimary,
+                ),
+              ),
+            ),
           ),
         ),
       ),
--- lib/features/on_boarding/widgets/on_boarding_page_widget.dart
+++ lib/features/on_boarding/widgets/on_boarding_page_widget.dart
@@ -24,33 +24,59 @@ class OnBoardingPageWidget extends StatelessWidget {
           ZoomIn(
             key: ValueKey('icon_${item.title}'),
             duration: const Duration(milliseconds: 600),
-            child: Container(
-              width: 180,
-              height: 180,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  colors: [
-                    colorScheme.primaryContainer,
-                    colorScheme.secondaryContainer,
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-                border: Border.all(
-                  color: colorScheme.primary.withValues(alpha: 0.2),
-                  width: 2,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: colorScheme.primary.withValues(alpha: 0.18),
-                    blurRadius: 36,
-                    spreadRadius: 4,
-                    offset: const Offset(0, 12),
+            child: Center(
+              child: Container(
+                width: 180,
+                height: 180,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: colorScheme.primary.withValues(alpha: 0.1),
+                  border: Border.all(
+                    color: colorScheme.primary.withValues(alpha: 0.25),
+                    width: 2,
                   ),
-                ],
+                  boxShadow: [
+                    BoxShadow(
+                      color: colorScheme.primary.withValues(alpha: 0.12),
+                      blurRadius: 30,
+                      spreadRadius: 4,
+                      offset: const Offset(0, 10),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: Container(
+                    width: 128,
+                    height: 128,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        colors: [
+                          colorScheme.primary,
+                          colorScheme.primary.withValues(alpha: 0.88),
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: colorScheme.primary.withValues(alpha: 0.35),
+                          blurRadius: 20,
+                          spreadRadius: 2,
+                          offset: const Offset(0, 6),
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Icon(
+                        item.icon,
+                        size: 96,
+                        color: colorScheme.onPrimary,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-              child: Icon(item.icon, size: 128, color: colorScheme.onPrimary),
             ),
           ),
 
